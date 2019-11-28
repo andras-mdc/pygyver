bf2177c33bb7b60de93fc378db0c651cacbac1db
Simone Fiorentini <37548538+bluefloyd00@users.noreply.github.com>
2019-11-28 21:38:02 +0000

    PR: add packaging (#3)
    * added packaging
    * applied changes from code review

---
+++ data_prep/test/test_tdd_utility.py
@@ -0,0 +1,23 @@
+
+import data_prep.tdd_utility as tu
+import unittest
+import json
+import filecmp as cmp
+
+class test_mockdata(unittest.TestCase):
+
+    def test_class_csv_from(self):
+        json_output = tu.load_csv(
+            csv="data_prep/test/data/csv/repeated_records.csv", 
+            schema="data_prep/test/data/schema/repeated_records_schema.json"
+        )
+        result = json_output.to_new_line_delimiter_file("data_prep/test/data/json/output.json")
+        self.assertTrue(result==0, "unit test to_new_line_delimiter_file() - return value")
+        self.assertTrue(
+            cmp.cmp(
+                "data_prep/test/data/json/output.json",
+                "data_prep/test/data/json/repeated_records_expected.json"), 
+            'unit test to_new_line_delimiter_file() - created file')
+        
+if __name__ == "__main__":
+    unittest.main()
